b2d6966c93212ec171ff7d052808b92e96707659
lucdon <lucdonders1@gmail.com>
2019-02-20 14:43:27 +0100

    optimize q4 a bit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 2id70Group13
+
+# possible improvements:
+- index on student gender
+
+## q4
+- 500 ms -> CREATE INDEX idx_dept ON StudentRegistrationsToDegrees(DegreeId, StudentId); 
--- q2Queries.sql
+++ q2Queries.sql
@@ -8,10 +8,16 @@
 -- q3:
 
 -- q4:
---EXPLAIN ANALYZE 
-WITH StudentsInDegree AS (SELECT st.StudentId, st.Gender FROM Students AS st, StudentRegistrationsToDegrees AS st2deg, Degrees AS deg WHERE st2deg.DegreeId=deg.DegreeId AND st.StudentId=st2deg.StudentId AND deg.Dept='thou shalt'),
-GenderPercentage AS (SELECT Gender, (100 * COUNT(*) / sum(COUNT(*)) OVER()) AS Percentage FROM StudentsInDegree GROUP BY Gender)
-SELECT Percentage FROM GenderPercentage WHERE Gender='F';
+
+CREATE INDEX idx_studentToDegree ON StudentRegistrationsToDegrees(DegreeId, StudentId);
+CREATE INDEX idx_gender ON Students(Gender);
+
+EXPLAIN ANALYZE 
+WITH StudentsInDegree AS (SELECT st.StudentId, st.Gender FROM Students AS st, StudentRegistrationsToDegrees AS st2deg, Degrees AS deg WHERE st2deg.DegreeId=deg.DegreeId AND st.StudentId=st2deg.StudentId AND deg.Dept='thou shalt')
+SELECT (100. * COUNT(*) / (SELECT COUNT(*) FROM StudentsInDegree)) AS Percentage FROM StudentsInDegree WHERE Gender='F';
+
+DROP INDEX idx_studentToDegree;
+DROP INDEX idx_gender;
 
 -- q5:
 
