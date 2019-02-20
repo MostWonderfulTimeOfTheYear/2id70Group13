2f6ed525a053941c047a90ef3827df20ff4ba87e
lucdon <lucdonders1@gmail.com>
2019-02-20 11:04:17 +0100

    create and load data queries

--- createTables.sql
+++ createTables.sql
@@ -3,4 +3,12 @@
 -- SQL files can be executed from command-line using psql -d THE_DATABASE_NAME -f THE_FILENAME
 -- Notice the UNLOGGED option, which drastically increases performance and reduces space usage
 
-CREATE UNLOGGED TABLE myPhonebook(id int, name varchar(50), address varchar(200), phoneNumber varchar(100));
+CREATE UNLOGGED TABLE IF NOT EXISTS Students(StudentId int PRIMARY KEY, StudentName varchar(50), Address varchar(200), BirthyearStudent int, Gender char);
+CREATE UNLOGGED TABLE IF NOT EXISTS Degrees(DegreeId int PRIMARY KEY, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int);
+CREATE UNLOGGED TABLE IF NOT EXISTS StudentRegistrationsToDegrees(StudentRegistrationId int PRIMARY KEY, StudentId int REFERENCES Students(StudentId), DegreeId int REFERENCES Degrees(DegreeId), RegistrationYear int);
+CREATE UNLOGGED TABLE IF NOT EXISTS Teachers(TeacherId int PRIMARY KEY, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char); 
+CREATE UNLOGGED TABLE IF NOT EXISTS Courses(CourseId int PRIMARY KEY, CourseName varchar(50), CourseDescription varchar(200), DegreeId int REFERENCES Degrees(DegreeId), ECTS int);
+CREATE UNLOGGED TABLE IF NOT EXISTS CourseOffers(CourseOfferId int PRIMARY KEY, CourseId int REFERENCES Courses(CourseId), Year int, Quartile int);
+CREATE UNLOGGED TABLE IF NOT EXISTS TeacherAssignmentsToCourses(CourseOfferId int REFERENCES CourseOffers(CourseOfferId), TeacherId int REFERENCES Teachers(TeacherId));
+CREATE UNLOGGED TABLE IF NOT EXISTS StudentAssistants(CourseOfferId int REFERENCES CourseOffers(CourseOfferId), StudentRegistrationId int REFERENCES StudentRegistrationsToDegrees(StudentRegistrationId));
+CREATE UNLOGGED TABLE IF NOT EXISTS CourseRegistrations(CourseOfferId int REFERENCES CourseOffers(CourseOfferId), StudentRegistrationId int REFERENCES StudentRegistrationsToDegrees(StudentRegistrationId), Grade int);
--- loadData.sql
+++ loadData.sql
@@ -8,6 +8,14 @@
 -- Don't forget to analyze at the end. It can make a difference in query performance.
 -- Example:
 
-COPY myPhonebook(id, name, address,phoneNumber) FROM '/mnt/ramdisk/tables/myphonebook.table' DELIMITER ',' CSV HEADER;
+COPY Students(StudentId, StudentName, Address, BirthyearStudent, Gender) FROM 'c:/dis/data/students.table' DELIMITER ',' CSV HEADER;
+COPY Degrees(DegreeId, Dept, DegreeDescription, TotalECTS) FROM 'c:/dis/data/degrees.table' DELIMITER ',' CSV HEADER;
+COPY StudentRegistrationsToDegrees(StudentRegistrationId, StudentId, DegreeId, RegistrationYear) FROM 'c:/dis/data/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER; 
+COPY Teachers(TeacherId, TeacherName, Address, BirthyearTeacher, Gender) FROM 'c:/dis/data/Teachers.table' DELIMITER ',' CSV HEADER;
+COPY Courses(CourseId, CourseName, CourseDescription, DegreeId, ECTS) FROM 'c:/dis/data/Courses.table' DELIMITER ',' CSV HEADER;
+COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM 'c:/dis/data/CourseOffers.table' DELIMITER ',' CSV HEADER;
+COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM 'c:/dis/data/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
+COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM 'c:/dis/data/StudentAssistants.table' DELIMITER ',' CSV HEADER;
+COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM 'c:/dis/data/CourseRegistrations.table' DELIMITER ',' CSV HEADER; 
 
 ANALYZE VERBOSE
