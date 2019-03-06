26b4ceed1fcb94ac4506704d5ee3d7432d7ac1e1
Kay <weijkai520@gmail.com>
2019-03-06 14:37:35 +0100

    xxx

--- Q2.sql
+++ Q2.sql
@@ -1,30 +1,4 @@
-CREATE MATERIALIZED VIEW excellentStudents(studentid,gpa) AS
-	With noFailure(studentid,degreeid) AS(
-			SELECT studentregistrationstodegrees.studentid,studentregistrationstodegrees.degreeid
-			FROM studentregistrationstodegrees,courseregistrations
-			WHERE studentregistrationstodegrees.studentregistrationid=courseregistrations.studentregistrationid
-			GROUP BY studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid
-			HAVING MIN(courseregistrations.grade)>=5),
-	maxGrade(studentid,degreeid,courseid,maxgrade,ect) AS(
-			SELECT studentregistrationstodegrees.studentid,studentregistrationstodegrees.degreeid,courseoffers.courseid,max(courseregistrations.grade),courses.ects
-			FROM studentregistrationstodegrees,courseregistrations,courses,courseoffers
-			WHERE studentregistrationstodegrees.studentregistrationid=courseregistrations.studentregistrationid AND
-			  courseoffers.courseofferid=courseregistrations.courseofferid AND courses.courseid=courseoffers.courseid AND 
-			  (studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid) IN (SELECT studentid,degreeid
-										FROM noFailure)
-			GROUP BY studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid,courseoffers.courseid,courses.ects),
-	nerdyStudents(studentid,degreeid,weightedtotalgrades,totalects) AS(
-	SELECT studentid,degreeid, SUM (maxgrade*ect) AS weightedtotalgrades, SUM (ect) AS totalects
-	FROM maxGrade
-	GROUP BY studentid, degreeid)
-	SELECT nerdyStudents.studentid,CAST (nerdyStudents.weightedtotalgrades AS FLOAT)/CAST(nerdyStudents.totalects AS FLOAT) as gpa
-	FROM nerdyStudents, degrees
-	WHERE nerdyStudents.degreeid=degrees.degreeid AND nerdyStudents.totalects>=degrees.totalects;
 SELECT DISTINCT (studentid)
 FROM excellentStudents
 WHERE excellentStudents.gpa >=%1%
 ORDER BY studentid;
-
-
-		
-		
