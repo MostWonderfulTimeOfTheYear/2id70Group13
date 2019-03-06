a48a4aa9e53cdd3a43f2356f7085978763d4f7b7
lucdon <lucdonders1@gmail.com>
2019-03-06 15:01:26 +0100

    merge queries to one file

--- q2Create.sql
+++ q2Create.sql
@@ -4,6 +4,50 @@
 -- Remember that the database (including the auxiliary structures) needs to be less than 11 GB.
 -- This file will be executed with postgres -d uni -f q2Create.sql 
 
+-- q2:
+CREATE MATERIALIZED VIEW excellentStudents(studentid,gpa) AS
+	With noFailure(studentid,degreeid) AS(
+			SELECT studentregistrationstodegrees.studentid,studentregistrationstodegrees.degreeid
+			FROM studentregistrationstodegrees,courseregistrations
+			WHERE studentregistrationstodegrees.studentregistrationid=courseregistrations.studentregistrationid
+			GROUP BY studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid
+			HAVING MIN(courseregistrations.grade)>=5),
+	maxGrade(studentid,degreeid,courseid,maxgrade,ect) AS(
+			SELECT studentregistrationstodegrees.studentid,studentregistrationstodegrees.degreeid,courseoffers.courseid,max(courseregistrations.grade),courses.ects
+			FROM studentregistrationstodegrees,courseregistrations,courses,courseoffers
+			WHERE studentregistrationstodegrees.studentregistrationid=courseregistrations.studentregistrationid AND
+			  courseoffers.courseofferid=courseregistrations.courseofferid AND courses.courseid=courseoffers.courseid AND 
+			  (studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid) IN (SELECT studentid,degreeid
+										FROM noFailure)
+			GROUP BY studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid,courseoffers.courseid,courses.ects),
+	nerdyStudents(studentid,degreeid,weightedtotalgrades,totalects) AS(
+	SELECT studentid,degreeid, SUM (maxgrade*ect) AS weightedtotalgrades, SUM (ect) AS totalects
+	FROM maxGrade
+	GROUP BY studentid, degreeid)
+	SELECT nerdyStudents.studentid,CAST (nerdyStudents.weightedtotalgrades AS FLOAT)/CAST(nerdyStudents.totalects AS FLOAT) as gpa
+	FROM nerdyStudents, degrees
+	WHERE nerdyStudents.degreeid=degrees.degreeid AND nerdyStudents.totalects>=degrees.totalects;
+
+-- q3:
+Create TEMP VIEW sufficientGrade(studentid,degreeid,courseid,ect) AS
+	SELECT studentregistrationstodegrees.studentid,studentregistrationstodegrees.degreeid,courseoffers.courseid,max(courseregistrations.grade),courses.ects
+	FROM studentregistrationstodegrees, courseregistrations,courses,courseoffers
+	WHERE studentregistrationstodegrees.studentregistrationid=courseregistrations.studentregistrationid AND
+		courses.courseid=courseoffers.courseid AND courseoffers.courseofferid=courseregistrations.courseofferid 
+	AND courseregistrations.grade>=5
+	GROUP BY studentregistrationstodegrees.studentid, studentregistrationstodegrees.degreeid,courseoffers.courseid,courses.ects;
+Create TEMP VIEW totalECTS(studentid,degreeid,totalECTS) AS
+	SELECT sufficientGrade.studentid,sufficientGrade.degreeid, SUM(sufficientGrade.ect) AS totalECTS
+	FROM sufficientGrade
+	GROUP BY sufficientGrade.studentid,sufficientGrade.degreeid;
+CREATE TEMP VIEW activeStudents(studentid,degreeid) AS 
+	SELECT totalECTS.studentid,degrees.degreeid
+	FROM totalECTS, degrees
+	WHERE totalECTS.degreeid=degrees.degreeid AND totalECTS.totalECTS<degrees.totalects;
+
+-- q4:
+CREATE INDEX idx_studentToDegree ON StudentRegistrationsToDegrees(DegreeId, StudentId);
+
 -- q6:
 CREATE MATERIALIZED VIEW ExcellentCourseStudents(StudentId, NumberOfCoursesWhereExcellent) AS
     WITH
--- q2Queries.sql
+++ q2Queries.sql
@@ -1,18 +1,41 @@
 -- q1:
 
+WITH studentGrades (studentregistrationid,courseofferid,grade) AS(
+    SELECT studentregistrationstodegrees.studentregistrationid,courseregistrations.courseofferid,courseregistrations.grade
+    FROM courseregistrations,studentregistrationstodegrees
+    WHERE studentregistrationstodegrees.studentregistrationid=courseregistrations.studentregistrationid AND courseregistrations.grade>5 
+	AND studentregistrationstodegrees.studentid=%1% AND studentregistrationstodegrees.degreeid=%2%)
+SELECT courses.coursename, studentGrades.grade
+FROM studentGrades, courses, courseoffers
+WHERE courseoffers.courseid=courses.courseid AND courseoffers.courseofferid=studentGrades.courseofferid 
+ORDER BY courseoffers.year, courseoffers.quartile,courseoffers.courseofferid;
+
 -- q2:
+
 SELECT DISTINCT (studentid)
     FROM excellentStudents
-        WHERE excellentStudents.gpa >=%1%
+        WHERE excellentStudents.gpa >=9.1
             ORDER BY studentid;
 
 -- q3:
 
+WITH totalStudents(degreeid,total) AS (
+	SELECT activeStudents.degreeid,COUNT(*)
+	FROM students,activeStudents
+	WHERE students.studentid=activeStudents.studentid
+	GROUP BY degreeid),
+totalFemaleStudents(degreeid,totalFemale) AS(
+	SELECT activeStudents.degreeid,COUNT(*)
+	FROM students,activeStudents
+	WHERE students.studentid=activeStudents.studentid AND students.gender='F'
+	GROUP BY degreeid)
+SELECT totalStudents.degreeiD,CAST(totalFemaleStudents.totalFemale AS FLOAT)/CAST(totalStudents.total AS FLOAT) AS percentage
+FROM totalStudents,totalFemaleStudents
+WHERE totalStudents.degreeid=totalFemaleStudents.degreeid
+ORDER BY totalStudents.degreeiD
+
 -- q4:
 
-CREATE INDEX idx_studentToDegree ON StudentRegistrationsToDegrees(DegreeId, StudentId);
-
-EXPLAIN ANALYZE 
 WITH 
     StudentsInDegree AS (
         SELECT st.StudentId, st.Gender 
@@ -23,13 +46,8 @@ SELECT (100. * COUNT(*) / (SELECT COUNT(*) FROM StudentsInDegree)) AS percentage
     FROM StudentsInDegree 
         WHERE Gender='F';
 
-DROP INDEX idx_studentToDegree;
-
 -- q5:
---CREATE INDEX idx_Grade ON CourseOffers(CourseOfferId, CourseId);
 
-
-EXPLAIN ANALYZE 
 WITH 
     CourseOfferGradesBelow AS (
         SELECT co.CourseId, COUNT(co.CourseId) AS Below 
@@ -49,26 +67,21 @@ SELECT cogb.CourseId, (100. * (coga.Above) / (cogb.Below + coga.Above)) as perce
             GROUP BY cogb.CourseId, coga.Above, cogb.Below 
                 ORDER BY cogb.CourseId;
 
-
---DROP INDEX idx_Grade;
 -- q6:
 
-EXPLAIN ANALYZE 
 SELECT StudentId, numberOfCoursesWhereExcellent 
     FROM ExcellentCourseStudents 
         WHERE numberOfCoursesWhereExcellent >= 2;
 
 -- q7:
 
-/*
 SELECT deg.DegreeId, st.BirthyearStudent, st.Gender, AVG(cr.Grade) 
     FROM Degrees AS deg, Students AS st, CourseRegistrations AS cr, StudentRegistrationsToDegrees AS st2deg 
         WHERE st2deg.StudentId=st.StudentId AND st2deg.StudentRegistrationId=cr.StudentRegistrationId AND st2deg.DegreeId=deg.DegreeId 
             GROUP BY CUBE (deg.DegreeId, st.BirthyearStudent, st.Gender);
-*/
+
 -- q8:
 
-/*
 SELECT c.courseName, co.year, co.quartile 
     FROM courses AS c, courseOffers AS co 
         WHERE c.courseId = co.CourseId AND 
@@ -82,4 +95,3 @@ SELECT c.courseName, co.year, co.quartile
                         WHERE cr.CourseOfferId=co.CourseOfferId and co.courseId = c.courseId
                 )/50
                 ORDER BY co.courseOfferId;
-*/
